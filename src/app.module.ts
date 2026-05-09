56f4b424922ceb90dc7777b4171ef4aecbd03904
tortikghst <kostilev.dimasergeevich@gmail.com>
2026-05-09 16:18:48 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,41 +1,23 @@
-
 import { Module } from '@nestjs/common';
-
 import { PrismaModule } from './prisma/prisma.module';
-
 import { AuthModule } from './auth/auth.module';
-
 import { UsersModule } from './users/users.module';
-
 import { EquipmentModule } from './equipment/equipment.module';
-
-import { OrdersModule } from './orders/orders.module';
-
-import { FavoritesModule } from './favorites/favorites.module';
-import { SupplierModule } from "./supplier/supplier.module";
-import { CategoriesModule } from "./categories/categories.module";
-
-
+// import { OrdersModule } from './orders/orders.module';
+// import { CategoriesModule } from './categories/categories.module';
+// import { SupplierModule } from './supplier/supplier.module';
+// import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-
-  imports: [CategoriesModule, SupplierModule, 
-
+  imports: [
     PrismaModule,
-
     AuthModule,
-
     UsersModule,
-
     EquipmentModule,
-
-    OrdersModule,
-
-    FavoritesModule,
-
+    // OrdersModule,
+    // CategoriesModule,
+    // SupplierModule,
+    // FavoritesModule,
   ],
-
 })
-
 export class AppModule {}
-
